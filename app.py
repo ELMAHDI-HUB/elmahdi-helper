531723acb302dd059de24655a11709ef3194e30c
ELMAHDI-HUB <mehdiotaku16@gmail.com>
2026-03-31 18:44:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from openai import OpenAI
 import os
 
+st.markdown(
+    '<meta name="google-site-verification" content="tUzfqfBAPloPFZwSRE_A8Ej3AmUziDvQ-msgf0VZNmU" />',
+    unsafe_allow_html=True
+)
 # 🔐 Get API key from Streamlit secrets
 API_KEY = st.secrets.get("NVIDIA_API_KEY")
 
